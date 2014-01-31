58fe5f0c5d755d3f1020e11b99c5db75ecb7bc34
woozie164 <woozie663@gmail.com>
2014-01-31 15:44:12 +0100

    GJorde så att man kan röra på kameran & gå runt.

---
+++ Projects/Assets/CameraMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	/// This script moves the character controller forward 
+	/// and sideways based on the arrow keys.
+	/// It also jumps when pressing space.
+	/// Make sure to attach a character controller to the same game object.
+	/// It is recommended that you make only one call to Move or SimpleMove per frame.	
+	float speed = 6.0f;
+	float jumpSpeed = 8.0f;
+	float gravity = 20.0f;
+	private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		//CharacterController controller = GetComponent(new CharacterController());
+		if (controller.isGrounded) {
+			// We are grounded, so recalculate
+			// move direction directly from axes
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,
+			                        Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			
+			if (Input.GetButton ("Jump")) {
+				moveDirection.y = jumpSpeed;
+			}
+		}
+		// Apply gravity
+		moveDirection.y -= gravity * Time.deltaTime;
+		
+		// Move the controller
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
