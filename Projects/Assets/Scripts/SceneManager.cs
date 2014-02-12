cd4ffc13ba9359a76596118937592bb9089ee6fa
woozie164 <woozie663@gmail.com>
2014-02-12 15:10:41 +0100

    Added class SceneManager
    SceneManager handles scene-changes. It currently loads the scene
    "Pac-Man Reloaded".
    
    SceneManager was called "Application" in the class-diagram, but because
    there's an already an application class in Unity, it needed another
    name.

---
+++ Projects/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The starting point of the game. It loads/unloads scenes.
+public class SceneManager : MonoBehaviour {
+	string initialScene = "Pac-Man Reloaded";
+
+	// Use this for initialization
+	void Start () {
+		Application.LoadLevel (initialScene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
