b44210012f96b9d6c5a618fdeb3cb7c4385acaee
Niklas Ulf Anders Cullberg <niklas.cullberg@hotmail.com>
2014-02-26 15:52:26 +0100

    I wrote a bit on a playerscript
    Checking to see if I can commit before doing anything major.

---
+++ Projects/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	int Lives = 3;
+	int Points = 0;
+	int PointMeasurer = 0;
+	int ExtraLifeLimit = 10000;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	int AddPoints (int points)
+	{
+		Points += points;
+		PointMeasurer += points;
+		if (PointMeasurer >= ExtraLifeLimit) 
+		{
+			Lives += 1;
+			PointMeasurer -= ExtraLifeLimit;
+		}
+		return Points;
+	}
+
+	bool Killed ()
+	{
+		Lives -= 1;
+		if (Lives <= 0) 
+		{
+			return true;
+		}
+		return false;
+	}
+}
