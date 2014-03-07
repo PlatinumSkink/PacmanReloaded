b7e1db36c9e42d5972005364ca1893d3ed83a938
woozie164 <woozie663@gmail.com>
2014-03-07 18:51:15 +0100

    Billboarding implemented

---
+++ Projects/Assets/Scripts/Billboarding.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This class will change the rotation towards the player.
+public class Billboarding : MonoBehaviour {
+	public Transform player;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt (player.transform.position);
+	}
+}
