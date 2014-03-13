011d653ebf7c9d702fabbb7e329a90050ec3ea8c
Niklas Ulf Anders Cullberg <niklas.cullberg@hotmail.com>
2014-03-13 16:07:25 +0100

    MOs+ @|)V@nc3D G|_|1 EVEr

---
+++ Projects/Assets/Scripts/GameGUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGUI : MonoBehaviour {
+
+	public Transform player;
+	public Transform level;
+
+	string life = "info";
+	string points = "";
+	string jumps = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		life = "Points: " + player.GetComponent<PlayerScript> ().Lives;
+		points = "Lives: " + player.GetComponent<PlayerScript> ().Points;
+		jumps = "Jumps: " + player.GetComponent<CameraMovement> ().jumpsRemaining;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (25, 25, 100, 30 * points.Length), points);
+		GUI.Label (new Rect (25, 50, 100, 30 * life.Length), life);
+		GUI.Label (new Rect (25, 75, 100, 30 * jumps.Length), jumps);
+	}
+}
--- Projects/Assets/Scripts/Ghost.cs
+++ Projects/Assets/Scripts/Ghost.cs
@@ -121,7 +121,6 @@ public class Ghost : MonoBehaviour {
 		{
 			currentDirection -= 4;
 		}
-		print (currentDirection);
 		SetDirection ();
 	}
 
